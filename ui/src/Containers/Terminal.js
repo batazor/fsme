6761dc0ff6c94a74367588cc44851faaeac93735
Victor Login <batazor111@gmail.com>
2019-02-18 16:42:09 +0300

    refactoring terminal

--- ui/src/Containers/Terminal.js
+++ ui/src/Containers/Terminal.js
@@ -28,16 +28,10 @@ class Command extends Component {
           backgroundColor='black'
           barColor='black'
           style={{ fontWeight: "bold", fontSize: "1em" }}
-          commands={{
-            'open-google': () => window.open('https://www.google.com/', '_blank'),
-            showmsg: this.showMsg,
-            popup: () => alert('Terminal in React')
-          }}
-          descriptions={{
-            'open-google': 'opens google.com',
-            showmsg: 'shows a message',
-            alert: 'alert', popup: 'alert'
-          }}
+          commands={{}}
+          descriptions={{}}
+          showActions={false}
+          hideTopBar
           msg='You can write anything here. Example - Hello! My name is Foo and I like Bar.'
         />
       </div>
