1d936bc18cd18dacbf109c8e500536116c8576db
Victor Login <batazor111@gmail.com>
2019-06-01 19:42:11 +0300

    fix

--- admin/ui/ops/root/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-envsubst '\$PORT' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf
-envsubst '\$HEROKU_PRIVATE_IP' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf
-nginx -g 'daemon off;'
